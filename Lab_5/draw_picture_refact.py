597aed6e72c926d8d9edece69e4aab11a3a2c4c9
VasyaSlot <Ivanov94VS@yandex.ru>
2021-09-16 00:52:54 +0300

    Add some new functions for refactoring

--- Lab_5/draw_picture_refact.py
+++ Lab_5/draw_picture_refact.py
@@ -1,5 +1,6 @@
 import pygame
 import numpy as np
+import math
 from pygame.draw import *
 
 def mirror_image_2D(coords_need_change, coords_of_center_symmetry = 500 ):
@@ -15,6 +16,109 @@ def mirror_image_2D(coords_need_change, coords_of_center_symmetry = 500 ):
                 list_coords_need_change = np.vstack((list_coords_need_change, [x_coords_new, coords[1]]))
             return list_coords_need_change
 
+
+def draw_man(scr, coord_min, coord_max):
+    '''
+
+    :param scr: screen where draw
+    :param coord_min: couple coordinates (x_min, y_min)
+    :param coord_max: couple coordinates (x_max, y_max)
+    :return:
+    '''
+    x_center = 0.5 * (coord_min[0] + coord_max[0])
+    y_center = 0.5 * (coord_min[1] + coord_max[1])
+    width = abs(coord_max[0] - coord_min[0])
+    height = abs(coord_max[1] - coord_min[1])
+
+    body_coords = (x_center, coord_max[1])
+    draw_body(scr, body_coords, 0.25 * width, YELLOW)
+
+    draw_head(scr, (x_center, y_center), 0.25 * abs(coord_max[0] - coord_min[0]), HUMAN)
+
+
+    #draw_arms()
+    #draw_hair()
+
+def draw_body(scr, body_coord_center, body_size, color):
+    circle(screen, color, body_coord_center, body_size)
+
+
+def draw_head(scr, head_coord_center, head_size, color):
+    '''
+    draw dace in scr
+    :param scr: screen where draw
+    :param coords: x,y-coords of center of face
+    :param size:size of face
+    :param color: color of face
+    :return:
+    '''
+    draw_face(scr, head_coord_center, head_size, color)
+
+    delta_eyes_coords = (0.3 * head_size, 0.3 * head_size)
+    draw_eyes(scr, head_coord_center, delta_eyes_coords, 0.2 * head_size, BLUE)
+
+    nose_size = (0.1 * head_size, 0.1 * head_size)
+    draw_nose(scr, head_coord_center, nose_size, BROWN)
+
+    mouth_size = (0.5 * head_size, 0.1 * head_size)
+    mouth_center_coords = (head_coord_center[0], head_coord_center[1] + 0.4 * head_size)
+    draw_mouth(scr, mouth_center_coords, mouth_size, RED)
+
+def draw_mouth(scr, mouth_center_coords, mouth_size, mouth_color):
+    mouth_coords = triangle_coords_up(mouth_center_coords, mouth_size)
+    polygon(scr, mouth_color, mouth_coords)
+
+def triangle_coords_up(triangle_center_coords, triangle_size):
+    return ((triangle_center_coords[0], triangle_center_coords[1] - triangle_size[1]),
+                    (triangle_center_coords[0] - triangle_size[0], triangle_center_coords[1] + triangle_size[1]),
+                    (triangle_center_coords[0] + triangle_size[0], triangle_center_coords[1] + triangle_size[1]),
+            )
+
+def triangle_coords_down(triangle_center_coords, triangle_size):
+    return ((triangle_center_coords[0], triangle_center_coords[1] + triangle_size[1]),
+                   (triangle_center_coords[0] + triangle_size[0], triangle_center_coords[1] - triangle_size[1]),
+                   (triangle_center_coords[0] - triangle_size[0], triangle_center_coords[1] - triangle_size[1]),
+                   )
+
+
+def draw_nose(scr, nose_center_coords, nose_size, nose_color):
+    '''
+    draw nose as right triangle
+    :param scr: screen object where need to draw nose
+    :param nose_center_coords: coords center nose
+    :param nose_size: size of triangle (from center ro vertice)
+    :param nose_color: color of nose
+    :return:
+    '''
+    nose_coords = triangle_coords_down(nose_center_coords, nose_size)
+    polygon(scr, nose_color, nose_coords)
+
+def draw_face(scr, face_coords, face_size, face_color):
+    circle(scr, face_color, face_coords, face_size)
+
+def draw_eyes(scr, face_coord_center, delta_coord_eyes, eye_size, eye_color):
+    coord_left_eye = (face_coord_center[0] - delta_coord_eyes[0], face_coord_center[1] - delta_coord_eyes[1])
+    circle(screen, eye_color, coord_left_eye, eye_size)
+    circle(screen, BLACK, coord_left_eye, 0.3 * eye_size)
+
+    coord_right_eye = (face_coord_center[0] + delta_coord_eyes[0], face_coord_center[1] - delta_coord_eyes[1])
+    circle(screen, eye_color, coord_right_eye, eye_size)
+    circle(screen, BLACK, coord_right_eye, 0.3 * eye_size)
+
+
+def draw_punch(scr, coords, size, color):
+    '''
+    draw punch in screen
+    :param scr: screen where draw
+    ::param coords: x,y-coords of center of punch
+    :param size: size of punch
+    :param color: color of punch
+    :return:
+    '''
+    #coords_left_punch_1 = np.array([[coords[1], coords[2]]])
+    circle(scr, color, (coords[0], coords[1]), size)
+
+
 #Инициализация библиотеки
 pygame.init()
 
@@ -29,21 +133,26 @@ HUMAN = (255, 223, 196)
 BROWN = (210, 105, 30)
 MAGENTA = (255, 0, 255)
 
+width = 2000
+height = 1000
 
 #Значение FPS для блавноого
 FPS = 30
 #Создание окна в котором будут картинки
-screen = pygame.display.set_mode((2000, 1000))
+screen = pygame.display.set_mode((width, height))
 screen.fill(WHITE)
 
 #Создаем кулаки
-coords_left_punch_1 = np.array([[90, 100]])
-circle(screen, HUMAN, coords_left_punch_1[0], 50)
-circle(screen, HUMAN, mirror_image_2D(coords_left_punch_1, coords_of_center_symmetry=500)[0], 50)
-#Создаем кулаки второго чела
-circle(screen, HUMAN, mirror_image_2D(coords_left_punch_1, coords_of_center_symmetry=1000)[0], 50)
-circle(screen, HUMAN, mirror_image_2D(mirror_image_2D(coords_left_punch_1, coords_of_center_symmetry=500), coords_of_center_symmetry=1000)[0], 50)
 
+#coords_left_punch_1 = np.array([[90, 100]])
+draw_punch(screen, (90, 100), 50, HUMAN)
+#circle(screen, HUMAN, coords_left_punch_1[0], 50)
+#draw_punch(screen, )
+#circle(screen, HUMAN, mirror_image_2D(coords_left_punch_1, coords_of_center_symmetry=500)[0], 50)
+#Создаем кулаки второго чела
+#circle(screen, HUMAN, mirror_image_2D(coords_left_punch_1, coords_of_center_symmetry=1000)[0], 50)
+#circle(screen, HUMAN, mirror_image_2D(mirror_image_2D(coords_left_punch_1, coords_of_center_symmetry=500), coords_of_center_symmetry=1000)[0], 50)
+'''
 #Создаем руки
 coords_arms = np.array([[79, 144], [106, 140], [287, 582], [263, 594]])
 polygon(screen, HUMAN, coords_arms)
@@ -144,7 +253,11 @@ coords_hairs = np.array([[331, 314],
 polygon(screen, MAGENTA, coords_hairs)
 
 #Создаем волосы 2 чела
-polygon(screen, MAGENTA, mirror_image_2D(coords_hairs, coords_of_center_symmetry=1000))
+polygon(screen, MAGENTA, mirror_image_2D(coords_hairs, coords_of_center_symmetry=1000))'''
+
+#Рисуем человека
+draw_man(screen, (0, 0.1 * height), (0.5 * width, height))
+
 
 #Создаем фон для надписи
 rect(screen, GREEN, (0, 0, 2000, 100))
