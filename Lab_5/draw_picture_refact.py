ed7568f09431d9e5f76f30e6ec84cee8f2849cb0
VasyaSlot <Ivanov94VS@yandex.ru>
2021-09-29 17:36:16 +0300

    End refactoring:

--- Lab_5/draw_picture_refact.py
+++ Lab_5/draw_picture_refact.py
@@ -46,7 +46,51 @@ def draw_man(scr, coord_min, coord_max):
     right_arm_coords_end = (coord_max[0] - 0.1 * width_man, coord_min[1])
     draw_arm(scr, right_arm_coords_start, right_arm_coords_end, GREEN, HUMAN)
 
-    #draw_hair()
+    draw_hair(scr, head_coords, 0.8 * body_size, YELLOW)
+
+def turn_point_arnd_center(point_turn_angle, start_coords, center_coords):
+
+    return [math.cos(point_turn_angle) * (start_coords[0] - center_coords[0]) -
+             math.sin( point_turn_angle) * (start_coords[1] - center_coords[1]) +
+             center_coords[0],
+            math.sin(point_turn_angle) * (start_coords[0] - center_coords[0]) +
+             math.cos(point_turn_angle) * (start_coords[1] - center_coords[1]) +
+             center_coords[1]]
+
+
+def draw_hair(scr, center_head_coords, head_radius, hair_color):
+    hair_start_coords = (center_head_coords[0] - math.cos(math.pi / 6) * head_radius,
+                         center_head_coords[1] - math.sin(math.pi / 6) * head_radius)
+    hair_angle = 120
+    hair_size = head_radius * 0.2
+    hairs_number = int(2 * math.pi * head_radius / 360 * hair_angle / hair_size)
+    turn_angle = hair_angle / hairs_number * math.pi / 180
+    L_center = head_radius + 3 ** 0.5 / 6 * hair_size
+    L_vertice = head_radius + 3 ** 0.5 / 2 * hair_size
+    print(L_vertice, L_center)
+    for i in range(hairs_number + 1):
+        center_bot_triangle_coords = turn_point_arnd_center(i * turn_angle, hair_start_coords, center_head_coords)
+
+        proection_vec = [(center_bot_triangle_coords[0] - center_head_coords[0]) /
+                         vector_length(center_head_coords, center_bot_triangle_coords),
+                        (center_bot_triangle_coords[1] - center_head_coords[1]) /
+                         vector_length(center_head_coords, center_bot_triangle_coords)]
+
+        triangle_center_coords = [L_center * proection_vec[0] + center_head_coords[0],
+                                    L_center * proection_vec[1] + center_head_coords[1]]
+        triangle_top_vertice_coords = [L_vertice * proection_vec[0] + center_head_coords[0],
+                                    L_vertice * proection_vec[1] + center_head_coords[1]]
+
+        polygon(scr, hair_color, right_triangle_coords(triangle_center_coords, triangle_top_vertice_coords))
+
+def vector_length(vec1, vec2):
+    return ((vec2[0] - vec1[0]) ** 2 + (vec2[1] - vec1[1]) ** 2) ** 0.5
+
+def right_triangle_coords(center_triangle_coords, vertice_coords):
+    triangle_turn_angle = 360 / 3 * math.pi / 180
+    return [turn_point_arnd_center(i * triangle_turn_angle, vertice_coords, center_triangle_coords)
+            for i in range(3)]
+
 
 
 
@@ -58,13 +102,7 @@ def draw_right_pentagon(pentagon_centr_coords, pentagon_radius):
                           )
 
     pentagon_angle = 360 / 5 * math.pi / 180
-
-    return [[math.cos(i * pentagon_angle) * (random_start_point[0] - pentagon_centr_coords[0]) -
-             math.sin(i * pentagon_angle) * (random_start_point[1] - pentagon_centr_coords[1]) +
-             pentagon_centr_coords[0],
-            math.sin(i * pentagon_angle) * (random_start_point[0] - pentagon_centr_coords[0]) +
-             math.cos(i * pentagon_angle) * (random_start_point[1] - pentagon_centr_coords[1]) +
-             pentagon_centr_coords[1]]
+    return [turn_point_arnd_center(i * pentagon_angle, random_start_point, pentagon_centr_coords)
             for i in range(5)]
 
 
@@ -181,121 +219,11 @@ FPS = 30
 screen = pygame.display.set_mode((width, height))
 screen.fill(WHITE)
 
-#Создаем кулаки
-
-#coords_left_punch_1 = np.array([[90, 100]])
-#draw_punch(screen, (90, 100), 50, HUMAN)
-#circle(screen, HUMAN, coords_left_punch_1[0], 50)
-#draw_punch(screen, )
-#circle(screen, HUMAN, mirror_image_2D(coords_left_punch_1, coords_of_center_symmetry=500)[0], 50)
-#Создаем кулаки второго чела
-#circle(screen, HUMAN, mirror_image_2D(coords_left_punch_1, coords_of_center_symmetry=1000)[0], 50)
-#circle(screen, HUMAN, mirror_image_2D(mirror_image_2D(coords_left_punch_1, coords_of_center_symmetry=500), coords_of_center_symmetry=1000)[0], 50)
-'''
-#Создаем руки
-coords_arms = np.array([[79, 144], [106, 140], [287, 582], [263, 594]])
-polygon(screen, HUMAN, coords_arms)
-polygon(screen, HUMAN, mirror_image_2D(coords_arms, coords_of_center_symmetry=500))
-polygon(screen, HUMAN, mirror_image_2D(coords_arms, coords_of_center_symmetry=1000))
-polygon(screen, HUMAN, mirror_image_2D(mirror_image_2D(coords_arms, coords_of_center_symmetry=500), coords_of_center_symmetry=1000))
-
-#Создаем тело
-coords_body = np.array([[500, 775]])
-circle(screen, YELLOW, coords_body[0], 220)
-
-#Создаем тело 2 чела
-circle(screen, YELLOW, mirror_image_2D(coords_body, coords_of_center_symmetry=1000)[0], 220)
-
-#Создаем плечи
-coords_shoulders = np.array([[230, 613], [318, 566], [364, 617], [319, 695], [236, 671]])
-polygon(screen, YELLOW, coords_shoulders)
-polygon(screen, BLACK, coords_shoulders, 1)
-
-polygon(screen, YELLOW, mirror_image_2D(coords_shoulders, coords_of_center_symmetry=500))
-polygon(screen, BLACK, mirror_image_2D(coords_shoulders, coords_of_center_symmetry=500), 1)
-
-#Создаем плечи 2 чела
-polygon(screen, YELLOW, mirror_image_2D(coords_shoulders, coords_of_center_symmetry= 1000))
-polygon(screen, BLACK, mirror_image_2D(coords_shoulders, coords_of_center_symmetry= 1000), 1)
-
-polygon(screen, YELLOW, mirror_image_2D(mirror_image_2D(coords_shoulders, coords_of_center_symmetry=500), coords_of_center_symmetry= 1000))
-polygon(screen, BLACK, mirror_image_2D(mirror_image_2D(coords_shoulders, coords_of_center_symmetry=500), coords_of_center_symmetry= 1000), 1)
-
-#Создаем лицо
-coords_face = np.array([[500, 420]])
-circle(screen, HUMAN, coords_face[0], 200)
-
-#Создаем лицо 2 чела
-circle(screen, HUMAN, mirror_image_2D(coords_face, coords_of_center_symmetry=1000)[0], 200)
-
-
-#Создаем глаза
-coords_eyes = np.array([[410, 390]])
-circle(screen, BLUE, coords_eyes[0], 30)
-circle(screen, BLUE, mirror_image_2D(coords_eyes, coords_of_center_symmetry = 500)[0], 30)
-
-#Создаем глаза 2 чела
-circle(screen, BLUE, mirror_image_2D(coords_eyes, coords_of_center_symmetry=1000)[0], 30)
-circle(screen, BLUE, mirror_image_2D(mirror_image_2D(coords_eyes, coords_of_center_symmetry = 500), coords_of_center_symmetry=1000)[0], 30)
-
-#Создаем зрачки
-coords_ineyes = np.array([[405, 390]])
-circle(screen, BLACK, coords_ineyes[0], 10)
-circle(screen, BLACK, mirror_image_2D(coords_ineyes, coords_of_center_symmetry = 500)[0], 10)
-
-#Создаем зрачки 2 чела
-circle(screen, BLACK, mirror_image_2D(coords_ineyes, coords_of_center_symmetry=1000)[0], 10)
-circle(screen, BLACK, mirror_image_2D(mirror_image_2D(coords_ineyes, coords_of_center_symmetry = 500), coords_of_center_symmetry=1000)[0], 10)
-
-#Создаем нос
-coords_noose = np.array([[485, 466], [500, 488], [515, 466]])
-polygon(screen, RED, coords_noose)
-
-#Создаем нос 2 чела
-polygon(screen, RED, mirror_image_2D(coords_noose, coords_of_center_symmetry=1000))
-
-#Создаем рот
-coords_mouth = np.array([[425, 530], [500, 570], [575, 530]])
-polygon(screen, BROWN, coords_mouth)
-
-#Создаем рот 2 чела
-polygon(screen, BROWN, mirror_image_2D(coords_mouth, coords_of_center_symmetry=1000))
-
-#Создаем волосы
-coords_hairs = np.array([[331, 314],
-                          [316, 281],
-                          [355, 282],
-                          [350, 239],
-                          [383, 255],
-                          [390, 211],
-                          [425, 233],
-                          [437, 192],
-                          [464, 223],
-                          [491, 191],
-                          [517, 219],
-                          [558, 200],
-                          [570, 232],
-                          [609, 219],
-                          [614, 255],
-                          [656, 244],
-                          [651, 287],
-                          [691, 285],
-                          [675, 318],
-                          [651, 287],
-                          [614, 255],
-                          [570, 232],
-                          [517, 219],
-                          [464, 223],
-                          [425, 233],
-                          [383, 255],
-                          [355, 282]])
-polygon(screen, MAGENTA, coords_hairs)
-
-#Создаем волосы 2 чела
-polygon(screen, MAGENTA, mirror_image_2D(coords_hairs, coords_of_center_symmetry=1000))'''
-
 #Рисуем человека
 draw_man(screen, (0, 0.1 * height), (0.5 * width, height))
+
+#Рисуем 2го человекаа
+draw_man(screen, (0.5 * width, 0.1 * height), (width, height))
 
 
 #Создаем фон для надписи
