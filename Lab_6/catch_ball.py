12df2c3e00a0f02d3fc0779989f85a74c8771cf7
VasyaSlot <Ivanov94VS@yandex.ru>
2021-10-06 03:05:51 +0300

    commit before test func event_gandler_for_catch_ball

--- Lab_6/catch_ball.py
+++ Lab_6/catch_ball.py
@@ -3,8 +3,12 @@ from pygame.draw import *
 from random import randint
 pygame.init()
 
-FPS = 2
-screen = pygame.display.set_mode((1200, 900))
+FPS = 1
+global width, height, text_range
+width = 1700
+height = 900
+text_range = 500
+screen = pygame.display.set_mode((width, height))
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -16,15 +20,12 @@ BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 def new_ball():
-    '''
-    create new_ball with random coordinates (x, y) and radius (r)
-    have random color
-    :return:
-    '''
+
     global x, y, r
-    x = randint(100,700)
-    y = randint(100,500)
-    r = randint(30,50)
+    r = randint(30, 50)
+    x = randint(0 + r, width - text_range - r)
+    y = randint(0 + r, height - r)
+
     color = COLORS[randint(0, 5)]
     circle(screen, color, (x, y), r)
 
@@ -34,18 +35,40 @@ def click(event):
 def vector_length(vec1, vec2):
     return ((vec2[0] - vec1[0]) ** 2 + (vec2[1] - vec1[1]) ** 2) ** 0.5
 
+def draw_print(scr, color_print, min_coords, max_coords, text_of_print):
+    font1 = pygame.font.Font(None, 125)
+
+    text1 = font1.render(text_of_print, True, BLACK)
+
+    text_rect = rect(scr, color_print, (min_coords[0], min_coords[1], max_coords[0], max_coords[1]))
+
+    scr.blit(text1, text_rect)
+
+def event_handler_for_catch_ball(evnt):
+    global score_count
+    guess_vec = (0, 0)
+
+    if evnt.button == 1:
+        guess_vec = pygame.mouse.get_pos()
+
+        if vector_length(guess_vec, (x, y)) <= r:
+            score_count += 1
+        else:
+            print('LOOOSER!!!')
+
+screen.fill(BLACK)
+
 pygame.display.update()
 clock = pygame.time.Clock()
-finished = False
-score_count = 0
 
+finished = False
+
+score_count = 0
 guess_vec = (0, 0)
+
 
 while not finished:
     clock.tick(FPS)
-
-    new_ball()
-
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -54,18 +77,16 @@ while not finished:
             if event.button == 1:
                 guess_vec = pygame.mouse.get_pos()
 
-    pygame.time.wait(3000)
+                if vector_length(guess_vec, (x, y)) <= r:
+                    score_count += 1
+                else:
+                    print('LOOOSER!!!')
 
-    print(guess_vec)
-    print((x, y))
 
-    if vector_length(guess_vec, (x, y)) <= r:
-        score_count += 1
-    else:
-        print('LOOOSER!!!')
 
-    print(score_count)
+    new_ball()
 
+    draw_print(screen, GREEN, (width - text_range, 0), (width, height), f"SCORE:" + str(score_count))
     pygame.display.update()
     screen.fill(BLACK)
 
